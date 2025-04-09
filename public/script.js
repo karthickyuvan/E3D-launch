faea44fcf5f6d45bc04f547bb7b5e6204ad051ed
karthickyuvan <alkarthickyuvan@gmail.com>
2025-04-09 14:18:55 +0530

    first commit

---
+++ public/script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById('subscribeForm');
+    const emailInput = document.getElementById('subscriberEmail');
+    const successMsg = document.getElementById('successMsg');
+  
+    form.addEventListener('submit', async function (e) {
+      e.preventDefault();
+  
+      const email = emailInput.value;
+  
+      try {
+        const response = await fetch('/subscribe', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ email })
+        });
+  
+        if (response.ok) {
+          successMsg.style.display = 'block';
+          emailInput.value = '';
+        } else {
+          alert("Something went wrong. Please try again.");
+        }
+      } catch (err) {
+        alert("Error connecting to server.");
+        console.error(err);
+      }
+    });
+  });
+  
